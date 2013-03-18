7e6e7cd0a2b0bb76fe03e09381229f0c5e97ea30
Aadil Shaik <aadilshaik@Aadils-MacBook-Air.local>
2013-03-18 17:38:21 +0530

    Initial Commit

---
+++ SampleNews5/AppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.h
+//  SampleNews5
+//
+//  Created by Aadil Shaik on 18/03/13.
+//  Copyright (c) 2013 Verve Infinite Developer Aadil Shaik. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class ViewController;
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) ViewController *viewController;
+
+@end
---
+++ SampleNews5/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  SampleNews5
+//
+//  Created by Aadil Shaik on 18/03/13.
+//  Copyright (c) 2013 Verve Infinite Developer Aadil Shaik. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
